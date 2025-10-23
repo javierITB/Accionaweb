cff00768afe0e21a7ee147b37d905413795ab470
javierITB <vicente.torresb@mail.udp.cl>
2025-10-23 13:19:09 -0300

    api

--- back_vercel/utils/validarToken.js
+++ back_vercel/utils/validarToken.js
@@ -1,6 +1,6 @@
 export async function validarToken(db, token) {
   const tokenData = await db.collection("tokens").find();
-  
+
   console.log("TOKEN DATA", tokenData)
   if (!tokenData) {
     return { ok: false, reason: "No existe" };
@@ -21,11 +21,11 @@ export async function validarToken(db, token) {
     creacion.getFullYear() === ahora.getFullYear() &&
     creacion.getMonth() === ahora.getMonth() &&
     creacion.getDate() === ahora.getDate();
-
+/*
   if (!mismoDia) {
     await db.collection("tokens").deleteOne({ token });
     return { ok: false, reason: "Antiguo" };
   }
-
+*/
   return { ok: true };
 }
