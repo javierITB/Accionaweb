ef80436247a6d75e954ded920a534153b0fd1f71
javierITB <vicente.torresb@mail.udp.cl>
2025-11-14 14:39:34 -0300

    intento de estadisticas de uso

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Routes from "./Routes";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
-    <>
-    <Routes />
-    </>
+    <Analytics>
+      <SpeedInsights>
+        <Routes />
+      </SpeedInsights>
+    </Analytics>
   );
 }
 
