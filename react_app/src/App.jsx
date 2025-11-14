1b983f73c9ff292d15d8afdf34e405737e590c53
javierITB <vicente.torresb@mail.udp.cl>
2025-11-14 16:18:37 -0300

    cambios

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Routes from "./Routes";
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
