88919e5b810fbd6fdbb917adb2de6d64d72b6fa2
javierITB <vicente.torresb@mail.udp.cl>
2025-11-17 10:00:39 -0300

    actualizacion api

--- back_vercel/endpoints/answers.js
+++ back_vercel/endpoints/answers.js
@@ -362,7 +362,7 @@ router.post("/chat", async (req, res) => {
         descripcion: `${autor} le ha enviado un mensaje respecto a un formulario.`,
         icono: "MessageCircle",
         color: "#45577eff",
-        actionUrl: `/?id=${result.insertedId}`,
+        actionUrl: `/?id=${respuesta.insertedId}`,
       });
     }
 
