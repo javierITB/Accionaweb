79f7f28790d7cfb963fd2d24317034a7ec995537
javierITB <vicente.torresb@mail.udp.cl>
2025-12-19 12:53:40 -0300

    aumento de volumen

--- react_app/src/components/ui/Header.jsx
+++ react_app/src/components/ui/Header.jsx
@@ -39,7 +39,7 @@ const Header = ({ className = '' }) => {
 
   // Efecto para el audio (opcional: ajustar volumen)
   useEffect(() => {
-    audioRef.current.volume = 0.75;
+    audioRef.current.volume = 1.5;
   }, []);
 
   useEffect(() => {
