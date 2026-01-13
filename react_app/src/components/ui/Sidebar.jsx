a31d0add73ab43e8fa21c44e7743a7be9f70414a
javierITB <vicente.torresb@mail.udp.cl>
2026-01-13 12:23:13 -0300

    cambio de nombre en panel de administración

--- react_app/src/components/ui/Sidebar.jsx
+++ react_app/src/components/ui/Sidebar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Icon from "../AppIcon";
-import Button from "./Button";
 
 const Sidebar = ({ isCollapsed = false, onToggleCollapse, className = "", isMobileOpen = false, onNavigate }) => {
 
