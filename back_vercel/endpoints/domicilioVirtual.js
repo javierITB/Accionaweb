8fd39488587f62f7392e1e6fb6d8d6f0b0e74dfd
Alejandro Lagos <alejandrodesss@gmail.com>
2026-01-15 15:57:08 -0300

    cambio para agregar el logo de acciona a formularios que vengan desde domicilio virtual x2

--- back_vercel/endpoints/domicilioVirtual.js
+++ back_vercel/endpoints/domicilioVirtual.js
@@ -822,7 +822,7 @@ router.post("/:id/regenerate-document", async (req, res) => {
                 form.section,
                 {
                     nombre: nombreUsuario,
-                    empresa: empresaUsuario,
+                    empresa: "Acciona Centro de Negocios Spa",
                     uid: uidUsuario,
                     mail: mailUsuario
                 },
