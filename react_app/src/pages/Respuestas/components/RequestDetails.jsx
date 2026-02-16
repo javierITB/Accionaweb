a744405cf5405b417805922ec2a59e03498cf513
Alejandro Lagos <alejandrodesss@gmail.com>
2026-02-16 17:28:19 -0300

    arreglos de button !["archivado", "finalizado"].includes(fullRequestData?.status)

--- react_app/src/pages/Respuestas/components/RequestDetails.jsx
+++ react_app/src/pages/Respuestas/components/RequestDetails.jsx
@@ -2393,7 +2393,7 @@ Máximo permitido: ${MAX_FILES} archivos.`;
                               )}
 
                            {/* BOTÓN "FINALIZAR" (Solo si está en revisión) */}
-                           {fullRequestData?.status !== "finalizado" && userPermissions?.finalize && (
+                           {!["archivado", "finalizado"].includes(fullRequestData?.status) && userPermissions?.finalize && (
                               <Button
                                  variant="default"
                                  iconName={isApproving ? "Loader" : "CheckCircle"}
